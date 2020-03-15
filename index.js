c7932130e24858ef782c125f217243f7e9cf786c
LOLSU <reinhardt.alexander@gmail.com>
2020-03-15 20:20:02 +0100

    not really final

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ console.log("Your local-ip adress is: "+ip.address() + "  Please naviagt to port
 
 http.createServer(function (req, res) { 
   if (req.url == '/fileupload') {
+    console.log();
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files, name) {
       var oldpath = files.filetoupload.path;
-      var newpath = os.homedir()+ '/Downloads/' + files.filetoupload._name;
+      var newpath = os.homedir()+ '/Downloads/' + files.filetoupload.name;
       fs.copyFile(oldpath, newpath, (err) => {
         if (err) throw err;
         console.log(oldpath + ' was copied to ' + newpath);
       });
       fs.unlink(oldpath, (err) => {
         if(err) throw err;
-        console.log('removed file');
       })
  });
   } else {
@@ -27,9 +27,7 @@ http.createServer(function (req, res) {
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<label>Select the file to upload here</label><br>');
     res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<label>Select the name for the file</label><br>');
-    res.write('<input type="text" name="_name"><br>');
-    res.write('<input type="submit">');
+    res.write('<input type="submit onlick="getIp()">');
     res.write('</form>');
     return res.end();
   }
